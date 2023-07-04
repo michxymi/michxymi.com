098665072ef8eee91470630af01e60d0f7bf38ab
michxymi <michxymi@gmail.com>
2023-07-04 21:24:21 +0100

    Use menu for socials in footer

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -31,13 +31,13 @@ function createMenuWith(classNames: string | undefined) {
 
 export default function NavBar() {
     return (
-        <nav className="navbar bg-base-100">
+        <nav className="navbar bg-base-200">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost sm:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    { createMenuWith("menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52")}
+                    { createMenuWith("menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52")}
                 </div>
             </div>
             <div className="navbar-center hidden sm:flex">
