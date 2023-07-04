fe03ffdd23bde71515ff5439184ea4a94ee4e0c9
michxymi <michxymi@gmail.com>
2023-07-04 20:22:55 +0100

    nav element inside navbar for consistency

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -28,21 +28,22 @@ function createMenuWith(classNames: string | undefined) {
         </ul>
     )
 }
+
 export default function NavBar() {
     return (
-        <div className="navbar bg-base-100">
+        <nav className="navbar bg-base-100">
             <div className="navbar-start">
                 <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                    <label tabIndex={0} className="btn btn-ghost sm:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     { createMenuWith("menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52")}
                 </div>
             </div>
-            <div className="navbar-center hidden lg:flex">
+            <div className="navbar-center hidden sm:flex">
                 {createMenuWith("menu menu-horizontal px-1")}
             </div>
             <div className="navbar-end"/>
-        </div>
+        </nav>
     )
 }
