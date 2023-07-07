9650ca37a1387764fbd36d65fa59c6ba24231ced
michxymi <michxymi@gmail.com>
2023-07-07 16:34:15 +0100

    add about page with placeholder text

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,15 @@
+import Avatar from "@/components/avatar"
+
 export default function About() {
   return (
-      <h1>About</h1>
+    <div className="min-h-screen bg-base-200">
+      <div className="flex flex-col w-full lg:flex-row">
+        <div className="grid flex-grow card rounded-box place-items-center m-8"><Avatar/></div> 
+        <div className="divider lg:divider-horizontal"/> 
+        <div className="grid flex-grow card rounded-box place-items-center">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
+    </div>
   )
 }
