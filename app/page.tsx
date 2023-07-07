bc1202a03b71b7ebe02619339fd6ec206bd2a4c5
michxymi <michxymi@gmail.com>
2023-07-07 16:33:44 +0100

    Make hero component a bit more dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import Hero from '@/components/hero'
+import Link from 'next/link'
 
 export default function Home() {
-  return <Hero/>
+  return (
+    <Hero>
+        <h1 className="text-5xl font-bold">Michael Xymitoulias</h1>
+        <p className="pt-6">Technical Software Manager</p>
+        <Link href="https://nanoporetech.com/" className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500">@Oxford Nanopore Technologies</Link>
+        <p className="pt-6">Code. Innovate. Lead.</p>
+    </Hero>
+  )
 }
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,14 +1,11 @@
 import Link from 'next/link'
 
-export default function Hero() {
+export default function Hero({children}: {children: React.ReactNode}) {
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content text-left">
                 <div className="max-w-md">
-                    <h1 className="text-5xl font-bold">Michael Xymitoulias</h1>
-                    <p className="pt-6">Technical Software Manager</p>
-                    <Link href="https://nanoporetech.com/" className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500">@Oxford Nanopore Technologies</Link>
-                    <p className="pt-6">Code. Innovate. Lead.</p>
+                    {children}
                 </div>
             </div>
         </div>
