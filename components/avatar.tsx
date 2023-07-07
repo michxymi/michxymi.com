aec05b7bafb1e5f579163250c745eb6b739eb3ed
michxymi <michxymi@gmail.com>
2023-07-07 16:33:08 +0100

    add avatar and headshot image

---
+++ components/avatar.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image"
+
+export default function Avatar(){
+    return (
+        <div className="avatar">
+            <div className="w-64 rounded-full">
+                <Image src="/headshot.jpg" width={512} height={512} alt="headshot" />
+            </div>
+        </div>
+    )
+}
