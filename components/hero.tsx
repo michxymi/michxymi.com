dc2fa06092ef3cda6bc55a3822c70e715403e7d0
michxymi <michxymi@gmail.com>
2023-07-07 15:07:00 +0100

    add tagline to homepage

--- components/hero.tsx
+++ components/hero.tsx
@@ -8,6 +8,7 @@ export default function Hero() {
                     <h1 className="text-5xl font-bold">Michael Xymitoulias</h1>
                     <p className="pt-6">Technical Software Manager</p>
                     <Link href="https://nanoporetech.com/" className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500">@Oxford Nanopore Technologies</Link>
+                    <p className="pt-6">Code. Innovate. Lead.</p>
                 </div>
             </div>
         </div>
