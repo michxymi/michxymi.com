c0e61cc3fe8e80ff36d33b71cd32059ef075ea0d
michxymi <michxymi@gmail.com>
2023-07-07 15:07:44 +0100

    navbar now shrinks on smaller screens

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 
-function createMenuWith(classNames: string | undefined) {
+function NavbarMenu(classNames: string) {
     const menuItems = [
         {
             label: "About",
@@ -34,14 +34,14 @@ export default function NavBar() {
         <nav className="navbar bg-base-200">
             <div className="navbar-start">
                 <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost sm:hidden">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    { createMenuWith("menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52")}
+                    { NavbarMenu("menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52")}
                 </div>
             </div>
-            <div className="navbar-center hidden sm:flex">
-                {createMenuWith("menu menu-horizontal px-1")}
+            <div className="navbar-center hidden lg:flex">
+                {NavbarMenu("menu menu-horizontal px-1")}
             </div>
             <div className="navbar-end"/>
         </nav>
