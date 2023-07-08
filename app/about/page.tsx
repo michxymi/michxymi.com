74a3a84c3667138c14f5f958c19b707e9c96883a
michxymi <michxymi@gmail.com>
2023-07-09 00:10:42 +0100

    Added short bio in about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,19 @@
 import Avatar from "@/components/avatar"
+import {Inter} from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function About() {
   return (
-    <div className="min-h-screen bg-base-200">
+    <div className="min-h-screen p-10 bg-base-200">
       <div className="flex flex-col w-full lg:flex-row">
-        <div className="grid flex-grow card rounded-box place-items-center m-8"><Avatar/></div> 
+        <div className="grid flex-grow h-auto card rounded-box place-items-center"><Avatar/></div> 
         <div className="divider lg:divider-horizontal"/> 
-        <div className="grid flex-grow card rounded-box place-items-center">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div className="grid flex-grow h-auto card rounded-box place-items-center">
+          <p>Welcome! I'm <strong>Michael Xymitoulias</strong> <em className={inter.className}>(/ksɪˌmiːˈtuːliəs/)</em>, a <strong>Software Engineer</strong> residing in the vibrant <strong>Oxfordshire</strong>.
+          <br/><br/>When I'm not <strong>leading teams</strong> and <strong>driving technical innovation</strong>,  you'll find me exploring the art of culinary delights or embracing the tranquility of the Aegean Islands alongside my cherished wife and daughter.
+          <br/><br/>With a <strong>passion for technology</strong> and a knack for <strong>problem-solving</strong>, I'm committed to delivering innovative solutions.
+          </p> 
         </div>
       </div>
     </div>
