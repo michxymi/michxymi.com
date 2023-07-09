209e15e7497f346d57b2d8e2f3b0f37cbfa73f69
michxymi <michxymi@gmail.com>
2023-07-09 22:15:15 +0100

    Add placeholder for articles route

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,5 +1,11 @@
 export default function Articles() {
     return (
-        <h1>Articles</h1>
+        <div className="min-h-screen bg-base-200">
+            <div className="flex h-screen">
+                <div className="m-auto">
+                    <h1>Exciting updates and industry insights are on the horizon!</h1>
+                </div>
+            </div>
+        </div>
     )
   }
