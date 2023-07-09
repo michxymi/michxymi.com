52e8d39e71e4d57f36c6c6af82fe7f4abb511a2a
michxymi <michxymi@gmail.com>
2023-07-09 10:47:11 +0100

    w-auto to avatar

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function Avatar(){
     return (
         <div className="avatar">
-            <div className="w-64 rounded-full">
+            <div className="w-auto rounded-full">
                 <Image src="/images/headshot.jpg" width={512} height={512} alt="headshot" />
             </div>
         </div>
