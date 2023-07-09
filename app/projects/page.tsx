5a92647cb244b70687e8d8d0015550cd4de35954
michxymi <michxymi@gmail.com>
2023-07-09 22:31:30 +0100

    Add placeholder for projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,11 @@
+export default function Projects(){
+    return (
+        <div className="min-h-screen bg-base-200">
+            <div className="flex h-screen">
+                <div className="m-auto">
+                    <h1>Stay tuned for an exciting showcase of my latest tech projects</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
