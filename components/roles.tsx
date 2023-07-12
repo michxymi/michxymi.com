d7ebc829a036386084838e62d3f6823bb7e88259
michxymi <michxymi@gmail.com>
2023-07-12 08:30:12 +0100

    Use card component for roles

--- components/roles.tsx
+++ components/roles.tsx
@@ -2,22 +2,22 @@ import { Key } from "react";
 
 export default function Roles({jobRoles} : {jobRoles: any}) {
     return (
-        <div className="shadow-lg rounded-lg p-6">
-          <h2 className="text-2xl font-bold mb-4">Professional Experience</h2>
-          <ul className="divide-y divide-primary-focus">
-            {jobRoles.map((jobRole: {title:string, company:string, duration:number, responsibilities:string[]}, index: Key | null | undefined ) => (
-              <li key={index} className="py-4">
-                <h3 className="text-lg font-medium">{jobRole.title}</h3>
-                <p className="">{jobRole.company}</p>
-                <p className="">{jobRole.duration}</p>
-                <ul className="list-disc list-inside pl-6">
-                  {jobRole.responsibilities.map((responsibility, index) => (
-                    <li key={index} className="">{responsibility}</li>
-                  ))}
-                </ul>
-              </li>
-            ))}
-          </ul>
-        </div>
-      );
-    };
+      <div className="p-6">
+        <h2 className="text-2xl font-bold mb-4">Work Experience</h2>
+        {jobRoles.map((jobRole: {title:string, company:string, duration:number, responsibilities:string[]}, index: Key | null | undefined ) => (
+          <div key={index} className="card bg-base-300 mb-4">
+            <div className="card-body">
+              <h3 className="text-lg font-medium mb-2">{jobRole.title}</h3>
+              <p className="mb-2">{jobRole.company}</p>
+              <p className="mb-4">{jobRole.duration}</p>
+              <ul className="list-disc list-inside pl-6">
+                {jobRole.responsibilities.map((responsibility, index) => (
+                  <li key={index}>{responsibility}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
