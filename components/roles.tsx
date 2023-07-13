774c064d2d3aea4c39039292aa13d65cc629d0ac
michxymi <michxymi@gmail.com>
2023-07-13 16:36:43 +0100

    integrate url from job roles in the component

--- components/roles.tsx
+++ components/roles.tsx
@@ -1,20 +1,16 @@
 import { Key } from "react";
+import Link from "next/link"
 
 export default function Roles({jobRoles} : {jobRoles: any}) {
     return (
       <div className="p-6">
         <h2 className="text-2xl font-bold mb-4">Professional Experience</h2>
-        {jobRoles.map((jobRole: {title:string, company:string, duration:number, responsibilities:string[]}, index: Key | null | undefined ) => (
+        {jobRoles.map((jobRole: {title:string, company:string, duration:number, url:string}, index: Key | null | undefined ) => (
           <div key={index} className="card bg-base-300 mb-4">
             <div className="card-body">
               <h3 className="text-lg font-medium mb-2">{jobRole.title}</h3>
-              <p className="mb-2">{jobRole.company}</p>
+              <Link href={jobRole.url} className="mb-2 underline underline-offset-4 hover:no-underline">{jobRole.company}</Link>
               <p className="mb-4">{jobRole.duration}</p>
-              <ul className="list-disc list-inside pl-6">
-                {jobRole.responsibilities.map((responsibility, index) => (
-                  <li key={index}>{responsibility}</li>
-                ))}
-              </ul>
             </div>
           </div>
         ))}
