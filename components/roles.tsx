2f7c879f4054b9519a40eb99fee619706a748dbb
Michael Xymitoulias <michael.xymitoulias@nanoporetech.com>
2023-07-13 14:56:22 +0100

    Change title

--- components/roles.tsx
+++ components/roles.tsx
@@ -3,7 +3,7 @@ import { Key } from "react";
 export default function Roles({jobRoles} : {jobRoles: any}) {
     return (
       <div className="p-6">
-        <h2 className="text-2xl font-bold mb-4">Work Experience</h2>
+        <h2 className="text-2xl font-bold mb-4">Professional Experience</h2>
         {jobRoles.map((jobRole: {title:string, company:string, duration:number, responsibilities:string[]}, index: Key | null | undefined ) => (
           <div key={index} className="card bg-base-300 mb-4">
             <div className="card-body">
