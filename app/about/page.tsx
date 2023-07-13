b26fcad7797c6bbe239cac887275aa9bbdf64694
michxymi <michxymi@gmail.com>
2023-07-13 16:36:22 +0100

    Remove responsibilities, add url instead for roles

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,58 +10,38 @@ const jobRoles = [
     title: 'Technical Software Manager',
     company: 'Oxford Nanopore Technologies',
     duration: 'Apr 2023 - Present',
-    responsibilities: [
-      'Manage a team of four integration engineers, overseeing tasks, objectives, and training.',
-      'Lead the management and integration of third-party packages into the C++ codebase.',
-      'Drive improvements in the CI/CD infrastructure while ensuring the adherence to best practices.',
-    ],
+    url: "https://nanoporetech.com/"
   },
   {
     title: 'Software Engineer & Section Leader',
     company: 'UK Atomic Energy Authority',
     duration: 'Mar 2022 - Apr 2023',
-    responsibilities: [
-      'Line manage a group of four multi-disciplinary engineers.',
-      'Core developer of the multi-award robotic middleware, CorteX.',
-      'Integration of robotic hardware and HMI development.',
-    ],
+    url: "https://www.gov.uk/government/organisations/uk-atomic-energy-authority"
   },
   {
     title: 'Control Systems Software Engineer',
     company: 'UK Atomic Energy Authority',
     duration: 'Dec 2017 - Mar 2022',
-    responsibilities: [
-      'Control system design for remote handling systems.',
-      'Performance analysis of control systems.',
-      'Key interface to external stakeholders ranging from small business to academic institutions.'
-    ],
+    url: "https://www.gov.uk/government/organisations/uk-atomic-energy-authority"
   },
   {
     title: 'Web Developer (Contract)',
     company: 'BubbleAd Ltd',
     duration: 'Mar 2017 - Jun 2017',
-    responsibilities: [
-      'Website development using the Wordpress platform.',
-      'Search Engine Optimization.',
-      'Support with various IT issues.'
-    ],
+    url: "https://www.facebook.com/bubbleadlondon/"
   },
   {
     title: 'Robotics Engineer (Internship)',
     company: 'Conferience Ltd',
     duration: 'Oct 2016 - Mar 2017',
-    responsibilities: [
-      'Embedded programming.',
-      'Electronic board design.',
-      'Web app development.'
-    ],
+    url: "https://conferience.com/"
   },
 ];
 
 export default function About() {
   return (
     <div className="min-h-screen bg-base-200 flex flex-col items-center justify-center p-10">
-      <div className="flex flex-col w-1/2 lg:flex-row">
+      <div className="flex flex-col lg:flex-row lg:w-1/2">
         <div className="grid flex-grow h-fit card rounded-box place-items-center">
           <div className="mb-4">
             <Avatar/>
@@ -73,23 +53,23 @@ export default function About() {
         <div className="divider lg:divider-horizontal"/>
         <div className="flex items-center justify-center">
           <div className="grid flex-grow h-fit card rounded-box text-justify">
-          <p className="leading-relaxed mb-4">
-            Welcome! I&apos;m <strong>Michael Xymitoulias</strong> <em className={inter.className}>(/ksɪˌmiː&apos;tuːliəs/)</em>,
+            <p className="leading-relaxed mb-4 tracking-tight">
+            Welcome! My name is <strong>Michael Xymitoulias</strong> <em className={inter.className}>(/ksɪˌmiː&apos;tuːliəs/)</em>,
             a <strong>Software Engineer</strong> residing in the vibrant <strong>Oxfordshire</strong>.
-          </p>
-          <p className="leading-relaxed mb-4">
-            When I&apos;m not <strong>leading teams</strong> and <strong>driving technical innovation</strong>,
-            you&apos;ll find me exploring the art of culinary delights or embracing the tranquility of the Aegean Islands
-            alongside my beautiful wife and daughter.
-          </p>
-          <p className="leading-relaxed mb-4">
-            With a <strong>passion for technology</strong> and a knack for <strong>problem-solving</strong>,
-            I&apos;m committed to delivering innovative solutions.
-          </p>
+            </p>
+            <p className="leading-relaxed mb-4 tracking-tight">
+              When I&apos;m not <strong>leading teams</strong> and <strong>driving technical innovation</strong>,
+              you&apos;ll find me exploring the art of culinary delights or embracing the tranquility of the Aegean Islands
+              alongside my beautiful wife and daughter.
+            </p>
+            <p className="leading-relaxed mb-4 tracking-tight">
+              With a <strong>passion for technology</strong> and a knack for <strong>problem-solving</strong>,
+              I&apos;m committed to delivering <strong>innovative</strong> solutions.
+            </p>
           </div>
         </div>
       </div>
-      <div className="min-h-screen flex justify-center p-10">
+      <div className="min-h-screen flex justify-center pt-10 pb-10">
         <Roles jobRoles={jobRoles} />
       </div>
     </div>
