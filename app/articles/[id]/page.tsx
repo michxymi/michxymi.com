db301a12612906bdd2f9e56b114e90305cfdc9be
michxymi <michxymi@gmail.com>
2023-07-16 09:35:41 +0100

    add bg color in articles

--- app/articles/[id]/page.tsx
+++ app/articles/[id]/page.tsx
@@ -10,7 +10,7 @@ export default async function Page({ params }: PageProps) {
   const postData = await getPostData(params.id)
   const { title, id, date, contentHtml} = postData
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <div className="flex justify-center items-center min-h-screen bg-base-200">
       <div className="max-w-screen-xl mx-auto px-4 py-8">
         <div className="mb-8">
           <h1 className="text-2xl font-bold mb-4 text-center">{title}</h1>
