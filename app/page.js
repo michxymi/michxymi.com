fc988cc3b7d7af11c553bca09a18ab96d0778671
michxymi <michxymi@gmail.com>
2023-09-01 15:02:31 +0100

    Refactor root page and layout

--- app/page.js
+++ app/page.js
@@ -1,13 +1,18 @@
-import Hero from '@/components/hero'
-import Link from 'next/link'
+import Link from "next/link";
+import Hero from "../components/Hero";
 
-export default function Home() {
-  return (
-    <Hero>
-        <h1 className="text-5xl font-bold">Michael Xymitoulias</h1>
-        <p className="pt-6">Technical Software Manager</p>
-        <Link href="https://nanoporetech.com/" className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500">@Oxford Nanopore Technologies</Link>
-        <p className="pt-6">Code. Innovate. Lead.</p>
-    </Hero>
-  )
-}
+const Home = () => (
+  <Hero>
+    <h1 className="text-5xl font-bold">Michael Xymitoulias</h1>
+    <p className="pt-6">Technical Software Manager</p>
+    <Link
+      href="https://nanoporetech.com/"
+      className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500"
+    >
+      @Oxford Nanopore Technologies
+    </Link>
+    <p className="pt-6">Code. Innovate. Lead.</p>
+  </Hero>
+);
+
+export default Home;
