52820b349cc22a413fac447cb4c67731cbfb6ff3
michxymi <michxymi@gmail.com>
2023-09-01 15:55:40 +0100

    Update Avatar component

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,11 +1,16 @@
-import Image from "next/image"
+import Image from "next/image";
 
-export default function Avatar(){
-    return (
-        <div className="avatar">
-            <div className="w-64 rounded-full">
-                <Image src="/images/headshot.jpg" width={512} height={512} alt="headshot" />
-            </div>
-        </div>
-    )
-}
+const Avatar = () => (
+  <div className="avatar">
+    <div className="w-64 rounded-full">
+      <Image
+        src="/images/headshot.jpg"
+        width={512}
+        height={512}
+        alt="headshot"
+      />
+    </div>
+  </div>
+);
+
+export default Avatar;
