22d21f9a19a9bc514329009134938e5a81342e0f
michxymi <michxymi@gmail.com>
2023-09-07 13:49:20 +0100

    Articles are now written in JS

--- app/articles/[id]/page.js
+++ app/articles/[id]/page.js
@@ -1,24 +1,23 @@
-// import { getPostData } from "@/lib/posts";
+import { getPostData } from "../../../lib/posts";
 
-// interface PageProps {
-//   params: {
-//     id: string;
-//   };
-// }
+const Article = async ({ params }) => {
+  const postData = await getPostData(params.id);
+  const { title, id, date, contentHtml } = postData;
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-base-200">
+      <div className="max-w-screen-xl mx-auto px-4 py-8">
+        <div className="mb-8">
+          <h1 className="text-2xl font-bold mb-4 text-center">{title}</h1>
+          <p className="mb-2 text-center">ID: {id}</p>
+          <p className="mb-2 text-center">Date: {date}</p>
+        </div>
+        <div
+          className="prose max-w-none"
+          dangerouslySetInnerHTML={{ __html: contentHtml }}
+        />
+      </div>
+    </div>
+  );
+};
 
-// export default async function Page({ params }: PageProps) {
-//   const postData = await getPostData(params.id)
-//   const { title, id, date, contentHtml} = postData
-//   return (
-//     <div className="flex justify-center items-center min-h-screen bg-base-200">
-//       <div className="max-w-screen-xl mx-auto px-4 py-8">
-//         <div className="mb-8">
-//           <h1 className="text-2xl font-bold mb-4 text-center">{title}</h1>
-//           <p className="mb-2 text-center">ID: {id}</p>
-//           <p className="mb-2 text-center">Date: {date}</p>
-//         </div>
-//         <div className="prose max-w-none" dangerouslySetInnerHTML={{ __html: contentHtml }} />
-//       </div>
-//     </div>
-//   );
-// }
+export default Article;
--- app/articles/page.js
+++ app/articles/page.js
@@ -1,39 +1,39 @@
-// import { getSortedPostsData } from "@/lib/posts";
-// import Link from "next/link";
+import { getSortedPostsData } from "../../lib/posts";
+import Link from "next/link";
 
-// export default function Articles() {
-//     const allPostsData = getSortedPostsData();
+const Articles = () => {
+  const allPostsData = getSortedPostsData();
 
-//     return (
-//       <div className="flex items-start justify-center min-h-screen bg-base-200">
-//         <div className="max-w-3xl">
-//           <p className="mt-8 mb-4 ml-4 leading-relaxed tracking-tight">
-//             Here, I share my thoughts and experiences on a wide range of topics,
-//             including <strong>software development</strong>, <strong>best practices</strong>, and insights into <strong>life matters</strong>.
-//             Join me on this journey as we explore the intersection of <strong>technology</strong>, <strong>personal growth</strong>, and the <strong>world around us</strong>.
-//           </p>
-//           <section className="p-4">
-//             <div className="grid grid-cols-1 gap-4">
-//               {allPostsData.map((postData) => (
-//                 <div className="bg-base-300 p-4 rounded" key={postData.id}>
-//                   <Link href={`/articles/${postData.id}`}><h3 className="text-lg font-semibold mb-2 underline underline underline-offset-4 hover:no-underline">{postData.title}</h3></Link>
-//                   <p className="text-sm mb-1">{postData.id}</p>
-//                   <p className="text-sm">{postData.date}</p>
-//                 </div>
-//               ))}
-//             </div>
-//           </section>
-//         </div>
-//       </div>
-// )}
-export default function Articles() {
   return (
-    <div className="min-h-screen bg-base-200">
-      <div className="flex h-screen">
-        <div className="m-auto">
-          <h1>Articles</h1>
-        </div>
+    <div className="flex items-start justify-center min-h-screen bg-base-200">
+      <div className="max-w-3xl">
+        <p className="mt-8 mb-4 ml-4 leading-relaxed tracking-tight">
+          Here, I share my thoughts and experiences on a wide range of topics,
+          including <strong>software development</strong>,{" "}
+          <strong>best practices</strong>, and insights into{" "}
+          <strong>life matters</strong>. Join me on this journey as we explore
+          the intersection of <strong>technology</strong>,{" "}
+          <strong>personal growth</strong>, and the{" "}
+          <strong>world around us</strong>.
+        </p>
+        <section className="p-4">
+          <div className="grid grid-cols-1 gap-4">
+            {allPostsData.map((postData) => (
+              <div className="bg-base-300 p-4 rounded" key={postData.id}>
+                <Link href={`/articles/${postData.id}`}>
+                  <h3 className="text-lg font-semibold mb-2 underline underline underline-offset-4 hover:no-underline">
+                    {postData.title}
+                  </h3>
+                </Link>
+                <p className="text-sm mb-1">{postData.id}</p>
+                <p className="text-sm">{postData.date}</p>
+              </div>
+            ))}
+          </div>
+        </section>
       </div>
     </div>
   );
-}
+};
+
+export default Articles;
