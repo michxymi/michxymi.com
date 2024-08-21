d6b9c6375c99663882b6cb7834c5d1b6d1609ea7
michxymi <michxymi@gmail.com>
2024-08-21 21:47:03 +0100

    added and configure content collections

---
+++ content-collections.ts
@@ -0,0 +1,20 @@
+import { defineCollection, defineConfig } from "@content-collections/core";
+ 
+const curriculum = defineCollection({
+  name: "curriculum",
+  directory: "./content/curriculum",
+  include: "*.json",
+  parser: "json",
+  schema: (z) => ({
+    company: z.string(),
+    role: z.string(),
+    location: z.string(),
+    startDate: z.coerce.date(),
+    endDate: z.optional(z.coerce.date()),
+    highlights: z.string().array(),
+  }),
+});
+ 
+export default defineConfig({
+  collections: [curriculum],
+});
