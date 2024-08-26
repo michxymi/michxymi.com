4eab8890c80a09274bcfdaae998d32e20a89513d
michxymi <michxymi@gmail.com>
2024-08-26 12:23:54 +0100

    add analytics and speed insights components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 import { ThemeProvider } from '@/components/theme-provider';
 import Footer from '@/components/footer';
 import { FloatingNav } from '@/components/ui/floating-navbar';
@@ -36,6 +38,8 @@ export default function RootLayout({
             </div>
           </main>
         </ThemeProvider>
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
